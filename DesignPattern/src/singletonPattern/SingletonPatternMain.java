f3947d408b20f2e18e2033cc86045c35ba4d14c8
TienDang <dangthimytien2311@gmail.com>
2018-03-26 11:59:31 +0700

    add singleton pattern

---
+++ DesignPattern/src/singletonPattern/SingletonPatternMain.java
@@ -0,0 +1,12 @@
+
+package singletonPattern;
+
+public class SingletonPatternMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SingleObject object = SingleObject.getInstance();
+		object.showMessage();
+	}
+
+}
